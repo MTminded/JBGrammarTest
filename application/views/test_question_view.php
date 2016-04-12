c35a7bb043c6c1a85c88b89a65daadd04f28354b
Mike Tang <mtminded@gmail.com>
2016-04-12 01:02:26 -0700

    Improved the UI, added the db file

--- application/views/test_question_view.php
+++ application/views/test_question_view.php
@@ -1,25 +1,82 @@
 <div class="content">
 	<div class="row mt40">
 		<div class="col-xs-12">
-			<h1><?php echo $num; ?></h1>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum quod facilis, eum necessitatibus similique possimus et aut tempora perspiciatis dolorem reiciendis, nulla obcaecati placeat, doloribus, impedit iure provident eligendi culpa!</p>
-			<pre>
-			<?php print_r($question); ?>
-			</pre>
-			
-			<div class="form_wrapper">
-			<?php echo form_open('test/submit') ?>
-				<input autocomplete="off" name="submitted_answer" value="<?php echo $answer;?>" placeholder="Type Answer" />
-				<input type="hidden" autocomplete="off" name="group_no" value=<?php echo $num; ?> />
-				<input type="hidden" autocomplete="off" name="question_no" value=<?php echo $question->No; ?> />
-				
-				<input type="submit" class="btn btn-default submit_button" name="submit" value="Next" />
+			<div class="box">
+				<div class="box_top">
+					
+					<div class="hud row">
+						<div class="col-xs-12 col-sm-9">
+							<div class="progress_bar">
+								<?php for ($x = 1; $x <= 16; $x++) :?>
+									<span class="progress-<?php echo $x;?> <?php if ($x<$num) echo "complete";?>"></span>
+								<?php endfor; ?>
+							</div>
+						</div>
 
-			</form>
+						<div class="col-xs-12 col-sm-3">
+							<div class="timer">
+								<i class="fa fa-clock-o" aria-hidden="true"></i> 15:00
+							</div>
+						</div>
+
+						<div class="col-xs-12"><div class="line"></div></div>
+					</div>
+					
+					<h1 class="title">Complete the sentence</h1>
+					<h2 class="subtitle">Select all answers that apply</h2>
+					
+					<div class="sentence"><?php echo $question->tests;?></div>
+					
+					<div class="clear">
+						<?php if ($question->choice_A != '') :?>
+							<label class="col-xs-6 col-sm-3 answer_wrapper">		 
+								<input class="answer" type="checkbox" name="answer_a" value="a"> <?php echo $question->choice_A;?>
+							</label>
+						<?php endif;?>	
+						<?php if ($question->choice_B != '') :?>
+							<label class="col-xs-6 col-sm-3 answer_wrapper">		 
+								<input class="answer" type="checkbox" name="answer_b" value="b"> <?php echo $question->choice_B;?>
+							</label>
+						<?php endif;?>	
+						<?php if ($question->choice_C != '') :?>
+							<label class="col-xs-6 col-sm-3 answer_wrapper">		 
+								<input class="answer" type="checkbox" name="answer_c" value="c"> <?php echo $question->choice_C;?>
+							</label>
+						<?php endif;?>	
+						<?php if ($question->choice_D != '') :?>
+							<label class="col-xs-6 col-sm-3 answer_wrapper">		 
+								<input class="answer" type="checkbox" name="answer_d" value="d"> <?php echo $question->choice_D;?>
+							</label>
+						<?php endif;?>	
+						<?php if ($question->choice_E != '') :?>
+							<label class="col-xs-6 col-sm-3 answer_wrapper">		 
+								<input class="answer" type="checkbox" name="answer_e" value="e"> <?php echo $question->choice_E;?>
+							</label>
+						<?php endif;?>	
+					</div>
+										
+					<div class="form_wrapper">
+					<?php echo form_open('test/submit') ?>
+						<input type="hidden" autocomplete="off" name="submitted_answer" value="<?php echo $answer;?>" placeholder="Type Answer" />
+						<input type="hidden" autocomplete="off" name="group_no" value=<?php echo $num; ?> />
+						<input type="hidden" autocomplete="off" name="question_no" value=<?php echo $question->No; ?> />
+						
+					</div>
+
+				</div>
+				<div class="box_bottom">
+					
+					<?php if ($num > 1):?>
+					<a href="<?php echo site_url('test/grammar/'. ($num-1) );?>" class="btn back_button f_l transition">Back</a>
+					<?php endif;?>
+
+					<button type="submit" class="btn btn-default next_button f_r transition" name="submit">Next</button>
+				</div>
+
+				</form>
+
 
 			</div>
-
-			<a class="btn btn-primary start_button" href="<?php echo site_url('test/grammar/');?>/<?php echo $num+1;?>"> Next Question</a>      
 		</div>
 		
 	</div>
